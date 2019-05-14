a9c83a5355d583caa6bd0bd1ad4db9927c9d4833
GarenMelikyan <garenmelikyan@gmail.com>
2019-05-14 21:49:00 +0100

    redesigned components

--- src/components/CharityDetail.js
+++ src/components/CharityDetail.js
@@ -1,66 +1,62 @@
-import React, { Component } from "react";
+import React from "react";
 import { Card, CardImg, CardBody, CardText, CardTitle } from "reactstrap";
 
-class CharityDetail extends Component {
-  renderComments(comments) {
-    if (comments == null) {
-      return <div />;
-    }
-    const list = comments.map(comment => {
-      return (
-        <li key={comment.id}>
-          <p>{comment.comment}</p>
-          <p>
-            -- {comment.author}, &nbsp;
-            {new Intl.DateTimeFormat("en-US", {
-              year: "numeric",
-              month: "long",
-              day: "2-digit"
-            }).format(new Date(comment.date))}
-          </p>
-        </li>
-      );
-    });
+const RenderComments = ({ comments }) => {
+  if (comments == null) {
+    return <div />;
+  }
+  const list = comments.map(comment => {
+    return (
+      <li key={comment.id}>
+        <p>{comment.comment}</p>
+        <p>
+          -- {comment.author}, &nbsp;
+          {new Intl.DateTimeFormat("en-US", {
+            year: "numeric",
+            month: "long",
+            day: "2-digit"
+          }).format(new Date(comment.date))}
+        </p>
+      </li>
+    );
+  });
+  return (
+    <div className="col-12 col-md-5 m-1">
+      <h4> Our Story => Visit our Shop</h4>
+      <ul className="list-unstyled">{list}</ul>
+    </div>
+  );
+};
+
+const RenderCharity = ({ charity }) => {
+  if (charity != null) {
     return (
       <div className="col-12 col-md-5 m-1">
-        <h4> Our Story => Visit our Shop</h4>
-        <ul className="list-unstyled">{list}</ul>
+        <Card>
+          <CardImg width="100%" src={charity.image} alt={charity.name} />
+          <CardBody>
+            <CardTitle>{charity.name}</CardTitle>
+            <CardText>{charity.description}</CardText>
+          </CardBody>
+        </Card>
       </div>
     );
+  } else {
+    return <div />;
   }
+};
 
-  renderDish(charity) {
-    if (charity != null) {
-      return (
-        <div className="col-12 col-md-5 m-1">
-          <Card>
-            <CardImg width="100%" src={charity.image} alt={charity.name} />
-            <CardBody>
-              <CardTitle>{charity.name}</CardTitle>
-              <CardText>{charity.description}</CardText>
-            </CardBody>
-          </Card>
-        </div>
-      );
-    } else {
-      return <div />;
-    }
+const CharityDetail = props => {
+  const charity = props.charity;
+  if (charity == null) {
+    return <div />;
   }
-
-  render() {
-    const charity = this.props.charity;
-    if (charity == null) {
-      return <div />;
-    }
-    const dishItem = this.renderDish(charity);
-    const commentItem = this.renderComments(charity.comments);
-    return (
-      <div className="row">
-        {dishItem}
-        {commentItem}
-      </div>
-    );
-  }
-}
+  return (
+    <div className="row">
+      <RenderCharity charity={props.charity} />
+      <RenderComments comments={props.charity.comments} />
+    </div>
+  );
+};
 
 export default CharityDetail;
--- src/components/Shops.js
+++ src/components/Shops.js
@@ -1,42 +1,32 @@
-import React, { Component } from "react";
+import React from "react";
 import { Card, CardImg, CardImgOverlay, CardTitle } from "reactstrap";
-import CharityDetail from "./CharityDetail";
 
-class Shops extends Component {
-  constructor(props) {
-    super(props);
+function RenderCharityItem({charity, onClick}) {
+  return (
+      <Card key={charity.id} onClick={() => onClick(charity.id)}>
+          <CardImg width="100%" src={charity.image} alt={charity.name} />
+          <CardImgOverlay>
+            <CardTitle>{charity.name}</CardTitle>
+          </CardImgOverlay>
+      </Card>
+  )
 
-    this.state = {
-      selectedDish: null
-    };
-  }
-
-  onSelectedDish(dish) {
-    this.setState({
-      selectedDish: dish
-    });
-  }
-
-  render() {
-    const menu = this.props.charities.map(charity => {
+}
+  
+const Shops = (props) => {
+    const menu = props.charities.map(charity => {
       return (
         <div key={charity.id} className="col-12 col-md-2 m-1">
-          <Card key={charity.id} onClick={() => this.props.onClick(charity.id)}>
-            <CardImg width="100%" src={charity.image} alt={charity.name} />
-            <CardImgOverlay>
-              <CardTitle>{charity.name}</CardTitle>
-            </CardImgOverlay>
-          </Card>
+          <RenderCharityItem charity = {charity} onClick ={props.onClick}/>
+        </div>
+        );
+      });
+      return (
+        <div className="container">
+          <div className="row">{menu}</div>
         </div>
       );
-    });
-    return (
-      <div className="container">
-        <div className="row">{menu}</div>
-        <CharityDetail charity={this.state.selectedDish} />
-      </div>
-    );
-  }
-}
+    }
 
+  
 export default Shops;
